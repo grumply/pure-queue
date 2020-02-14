abe2ad98454c5a485c599a0cc469ee53fe7d96a7
Sean Hickman <sean@grumply.com>
2020-02-13 23:34:40 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, stdenv }:
 mkDerivation {
   pname = "pure-queue";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base ];
   homepage = "github.com/grumply/pure-queue";
